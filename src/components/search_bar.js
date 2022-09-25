5ee5eaa529620608632d12c03f34cff97ccc1eb2
Regina Yan <38498065+regina-yan@users.noreply.github.com>
2022-09-25 16:52:00 -0500

    videoplayer

---
+++ src/components/search_bar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchterm: '' };
+    // this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  onInputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ searchterm: event.target.value });
+    this.props.onSearchChange(event.target.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.onInputChange} value={this.state.searchterm} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.scss';
+import debounce from 'lodash.debounce';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-function App() {
-  return <div className="test">REACT starterpack</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      // eslint-disable-next-line react/no-unused-state
+      selectedVideo: null,
+    };
+    this.search = debounce(this.search, 1000);
+    this.search('pixar');
+  }
+
+  search = (text) => {
+    youtubeSearch(text).then((videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar id="search-bar" onSearchChange={this.search} />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
 }
+
+// export default App;
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
